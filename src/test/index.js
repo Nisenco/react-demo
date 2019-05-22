a759e6a098611c2ceadd81dc5ea5774dec5b8ffd
nico <wangmeng1@social-touch.com>
2019-05-22 10:31:52 +0800

    init

---
+++ src/test/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+				<div>nihao</div>
+			)
+		
+	}
+};
+export default Test;
