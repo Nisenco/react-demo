59fa20de4a59206242a593c1ee574597810628fb
nico <wangmeng1@social-touch.com>
2019-06-05 14:34:45 +0800

    dev

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 // import Test from './test/index.js';
-import ClickCounter from './components/ClickCounte/ClickCounte.js';
+import ClickCounter from 'components/ClickCounte/ClickCounte.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/*<Test/>*/}
-        <ClickCounter></ClickCounter>
+        <ClickCounter />
       </div>
     );
   }
