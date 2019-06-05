ea68ac4aa68c840a92f494ebc368f61376b76776
nico <wangmeng1@social-touch.com>
2019-06-05 11:53:40 +0800

    redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-import Test from './test/index.js';
+// import Test from './test/index.js';
+import ClickCounter from './components/ClickCounte/ClickCounte.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Test/>
+        {/*<Test/>*/}
+        <ClickCounter></ClickCounter>
       </div>
     );
   }
---
+++ src/components/ClickCounte/ClickCounte.js
@@ -0,0 +1,26 @@
+import React, {PureComponent} from 'react';
+
+class  ClickCounter extends PureComponent{
+    constructor(props){
+        super(props);
+        this.onClickButton = this.onClickButton.bind(this);
+        this.state={
+            count:0
+        }
+    }
+    onClickButton(){
+        this.setState({
+            count:this.state.count +1
+        })
+    }
+    render() {
+        const {count} = this.state;
+        return (<div>
+            <button onClick={this.onClickButton}>点击</button>
+            <div>
+                click count:{count}
+            </div>
+        </div>)
+    }
+}
+export default ClickCounter;
