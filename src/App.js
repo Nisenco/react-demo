b7928efe3749b567b9f3205ba5f0f2d2fa2bc169
nico <wangmeng1@social-touch.com>
2019-06-11 09:55:09 +0800

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
-// import Test from 'components/test/index.js';
-// import ClickCounter from 'components/ClickCounte/ClickCounte.js';
-import BackTop from 'components/BackTop/BackTop.js'; // 回到顶点
-import MultiSelect from 'components/MultiSelect/MultiSelect.js';
+import {Provider} from 'react-redux';
+import TodoApp from 'components/TodoApp.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<Test/>*/}
-        {/*<ClickCounter />*/}
-        <BackTop />
-        <div style={{padding:'100px'}}>
-          <MultiSelect title="趋势类型"/>
-        </div>
-
+          <div style={{padding:'100px'}}>
+              <Provider>
+                  <TodoApp />
+              </Provider>
+          </div>
       </div>
     );
   }
---
+++ src/components/TodoApp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import
+function TodoApp(){
+    return (<div>
+
+    </div>)
+}
+export default TodoApp;
