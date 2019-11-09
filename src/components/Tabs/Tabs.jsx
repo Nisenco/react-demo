d04e9fbea53eb2d518bd35c013e07c6320f124ea
Nisenco <SUCCESSW2M@163.com>
2019-11-09 23:50:59 +0800

    更改tabs 文件

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { PureComponent } from "react";
 class Tabs extends PureComponent {
     constructor(props){
@@ -29,37 +28,3 @@ class Tabs extends PureComponent {
 }
 
 export default Tabs;
-=======
-import React,{PureComponent} from 'react';
-class Tabs extends PureComponent{
-    constructor(props){
-        super(props);
-        this.state = {
-
-        }
-    }
-    render(){
-        return (<div>
-            <div class="tabs-bar" role="tablist">
-                <ul class="tabs-nav">
-                    <li role="tab" class="tabs-tab">Tab 1</li>
-                    <li role="tab" class="tabs-tab">Tab 2</li>
-                    <li role="tab" class="tabs-tab">Tab 3</li>
-                </ul>
-            </div>
-            <div class="tabs-content">
-                <div role="tabpanel" class="tabs-panel">
-                    第一个 Tab 里的内容
-                </div>
-                <div role="tabpanel" class="tabs-panel">
-                    第二个 Tab 里的内容
-                </div>
-                <div role="tabpanel" class="tabs-panel">
-                    第三个 Tab 里的内容
-                </div>
-            </div>
-        </div>)
-    }
-}
-export default Tabs;
->>>>>>> 6763dad793fd4bbae692b9b22080f03477cd4b48
