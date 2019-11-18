5a31014d94e588a76e85eb63dbb3c89a93828e76
Nisenco <SUCCESSW2M@163.com>
2019-11-18 23:43:25 +0800

    switch

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,31 @@
 import React from "react";
 import {
 	BrowserRouter as Router,
-	// Switch,
+	Switch,
 	Route,
-	// Link,
+	Link,
 	useParams
 } from "react-router-dom";
 import Home from "../views/Home/Home.js";
 import Detail from "../views/Detail/Detail.js";
 const Routes = () => {
 	return (
-		<Router>
-			<Route path='/' component={Home} />
-			<Route path='/detail' component={Detail} />
-		</Router>
+		<div>
+			<Router>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/detail'>Detail</Link>
+					</li>
+				</ul>
+				<Switch>
+					<Route path='/' > <Home /> </Route>
+					<Route path='/detail' ><Detail /></Route>
+				</Switch>
+			</Router>
+		</div>
 	)
 }
 export default Routes;
