9d52c6d4de734d555ab885aa19082542daef2650
Nisenco <SUCCESSW2M@163.com>
2019-11-18 21:35:54 +0800

    Quill

---
+++ src/containers/Quill/index.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import './Quill.scss';
+
+class Quill extends PureComponent{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(<div>
+
+    </div>)
+  }
+}
+export default Quill;
