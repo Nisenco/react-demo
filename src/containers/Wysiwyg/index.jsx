8e84ecd61ac7dad71edc6ace6aad48cbd5f8cf36
Nisenco <SUCCESSW2M@163.com>
2019-12-09 21:50:44 +0800

    【dev】

--- src/containers/Wysiwyg/index.jsx
+++ src/containers/Wysiwyg/index.jsx
@@ -1,10 +1,10 @@
 import React, { PureComponent } from 'react';
-import FroalaEditor from 'react-froala-wysiwyg';
-import Froalaeditor from 'froala-editor';
-import FroalaEditorImg from 'react-froala-wysiwyg/FroalaEditorImg';
+// import FroalaEditor from 'react-froala-wysiwyg';
+// import Froalaeditor from 'froala-editor';
+// import FroalaEditorImg from 'react-froala-wysiwyg/FroalaEditorImg';
 
-import 'froala-editor/css/froala_style.min.css';
-import 'froala-editor/css/froala_editor.pkgd.min.css';
+// import 'froala-editor/css/froala_style.min.css';
+// import 'froala-editor/css/froala_editor.pkgd.min.css';
 
 // import { getID } from '../../assets/utills/help';
 import './Wysiwyg.css';
@@ -28,26 +28,26 @@ class CusWysiwyg extends PureComponent {
     });
   }
   componentDidMount(){
-    Froalaeditor.DefineIcon('insert', {NAME: 'plus', SVG_KEY: 'add'});
-    Froalaeditor.RegisterCommand('insert', {
-      title: 'Insert HTML',
-      focus: true,
-      undo: true,
-      refreshAfterCallback: true,
-      callback: function () {
-        this.html.insert('My New HTML');
-      }
-    });
+    // Froalaeditor.DefineIcon('insert', {NAME: 'plus', SVG_KEY: 'add'});
+    // Froalaeditor.RegisterCommand('insert', {
+    //   title: 'Insert HTML',
+    //   focus: true,
+    //   undo: true,
+    //   refreshAfterCallback: true,
+    //   callback: function () {
+    //     this.html.insert('My New HTML');
+    //   }
+    // });
   }
   render() {
     const {id} = this.state;
     return (<div id = {id}>
-      <FroalaEditor 
+      {/* <FroalaEditor 
         tag='textarea'
         config={this.config}
         model={this.state.model}
         onModelChange={this.handleModelChange}
-      />
+      /> */}
     </div>)
   }
 }
--- src/views/Detail/Detail.js
+++ src/views/Detail/Detail.js
@@ -1,12 +1,25 @@
 import React,{PureComponent} from 'react';
+import {Select} from 'antd';
+import './index.css';
+const Option  = Select.Option;
 class Detail extends  PureComponent{
     constructor(props){
         super(props);
-    }
+        this.state={
 
+        }
+    }
+    handleChange = (value)=>{
+        console.log(value,'value ++++++=');
+    }
     render(){
-        return (<div>
-            detail
+        return (<div className="detail_wraper">
+            <Select  placeholder="请输入值" style={{ width: 120 }} onChange={this.handleChange}>
+                <Option value="jasdfsdfck">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled" >Disabled</Option>
+                <Option value="Yiminghe">yiminghe</Option>
+            </Select>
         </div>)
     }
 }
