ee574be033cc47894760f017063c8762e08239fc
Nicosen <SUCCESSW2M@163.com>
2021-01-29 19:41:53 +0800

    d3

--- src/views/D3/index.js
+++ src/views/D3/index.js
@@ -7,43 +7,86 @@ import './d3.css';
 class D3Demo extends PureComponent {
   constructor(props) {
     super(props);
-    this.data =  [];
+    this.data = [];
     this.timer = null;
+    this.width = 500;
+    this.height = 500;
+    this.offset = 50;
+    this.margin = 25;
+    this.axisWidth = this.width - 2 * this.margin;
   }
-  componentDidMount() {
-    this.dataRender(this.data);
-    // if(this.timer){
-    //   clearInterval(this.timer);
-    // }
-    // this.timer = setInterval(()=>{
-    //   this.dataRender(this.data);
-    // },1500)
+  componentDidMount(){
+    // this.renderAll();
+    this.renderXAxis();
+    this.renderYAxis();
   }
-  datum = (x)=> 15 + x*x
-  newData = ()=>{
-    this.data.push(this.datum);
-    return this.data;
+  // 自定义刻度
+  renderAll = () => {
+    this.svg = d3.select('div#drawBar').append('svg')
+      .attr('class','axis')
+      .attr('width',this.width)
+      .attr('height',this.height);
+    this.scale = d3.scaleLinear().domain([0,1]).range([0,this.axisWidth]);
+    var axis = d3.axisBottom()
+      .scale(this.scale)
+      .ticks(10)
+      .tickSize(12)
+      .tickPadding(10)
+      .tickFormat(d3.format(".0%"));
+    this.svg.append('g')
+      .attr('transform',()=>{
+        return `translate(${this.margin},${this.margin})`;
+      })
+      .call(axis);
   }
-  dataRender = (data)=>{
-    const drawBar = d3.select('div#drawBar')
-                      .selectAll('div')
-                      .data(this.newData);
-      drawBar.enter().append('div').append('span');
-      drawBar.attr('class','v-bar')
-        .style('height',(d,i)=>{
-          return d(i) + 'px';
-        })
-        .select('span')
-        .text((d,i)=>{
-          return d(i);
-        })
-      drawBar.exit().remove();
+  // 绘制表格线
+  renderXAxis = ()=>{
+    this.svg = d3.select('div#drawBar1').append('svg')
+      .attr('class','axis')
+      .attr('width',this.width)
+      .attr('height',this.height);
+    var axisLength = this.width -2* this.margin;
+    var scale = d3.scaleLinear()
+        .domain([0,100])
+        .range([0,axisLength]);
+    var aAxis = d3.axisBottom().scale(scale);
+    this.svg.append('g')
+      .attr('class','x-axis')
+      .attr('transform',()=>{
+        return `translate(${this.margin},${this.height-this.margin})`;
+      })
+      .call(aAxis);
+      d3.selectAll('g.x-axis g.tick')
+        .append('line')
+        .classed('grid-line',true)
+        .attr('x1',0)
+        .attr("y1", 0)
+        .attr("x2", 0)
+        .attr("y2", - (this.height - 2 * this.margin));
+  }
+  renderYAxis = ()=>{
+    var axisLength = this.height-2*this.margin;
+    var scale = d3.scaleLinear().domain([100,0]).range([0,axisLength]);
+    var yAxis = d3.axisLeft().scale(scale);
+    this.svg.append('g')
+    .attr('class','y-axis')
+    .attr('transform',()=>{
+      return `translate(${this.margin},${this.height-this.margin})`;
+    })
+    .call(yAxis)
+    d3.selectAll('g.y-axis g.tick')
+    .append('line')
+    .classed("grid-line", true)
+    .attr("x1", 0)
+    .attr("y1", 0)
+    .attr("x2", axisLength) // <-F
+    .attr("y2", 0);
   }
   render() {
     return (
       <div className="d3" >
-        <div id="drawBar">
-        </div>
+        <div id="drawBar"></div>
+        <div id="drawBar1"></div>
       </div>)
   }
 }
