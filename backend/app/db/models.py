d0577068ac9750e139cface3362808bc92027598
Nicosen <SUCCESSW2M@163.com>
2021-03-25 20:29:41 +0800

    '文档'

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -6,8 +6,9 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=True)
-    first_name = Column(String)
-    last_name = Column(String)
+    username = Column(String)
+    create_time = Column(String,nullable=False)
+    loginTime = Column(String,nullable=False)
     hashed_password = Column(String, nullable=True)
     is_active = Column(Boolean, nullable=True)
     is_superuser = Column(Boolean, nullable=True)
